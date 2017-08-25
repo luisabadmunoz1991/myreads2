123f8117c435256fbfab3013ffe9463c632e681f
luisabadmunoz1991 <31046336+luisabadmunoz1991@users.noreply.github.com>
2017-08-25 12:54:36 -0500

    update

--- src/Book.js
+++ src/Book.js
@@ -1,8 +1,22 @@
 import React , {Component} from 'react'
 
 class Book extends Component{
+  constructor(props) {
+    super(props);
+    this.state = this.props.book.shelf;
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+    handleChange(event) {
+    this.setState(event.target.shelf);
+
+  }
+
 
    render() {
+
   return (
 
          <div className="book"> 
@@ -11,11 +25,11 @@ class Book extends Component{
                         <div className="book-cover" style={{width: 128, height:193, backgroundImage:`url(${this.props.book.imageLinks.thumbnail})`}}>
                          
                          </div>
-                              <div className="book-shelf-changer">
+                              <div className="book-shelf-changer" onSubmit={this.handleSubmit}>
 
-                              <select >
+                              <select value={this.state.shelf} onChange={this.handleChange}>
                                 <option value="none" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="currentlyReading" >Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
                                 <option value="read">Read</option>
                                 <option value="none">None</option>
