561b00719a41392d52427a90f8c78e4becab0fdd
luisabadmunoz1991 <31046336+luisabadmunoz1991@users.noreply.github.com>
2017-09-02 11:53:32 -0500

    upload

--- src/Listbooks.js
+++ src/Listbooks.js
@@ -30,9 +30,7 @@ class ListBooks extends Component{
       else {
             showingBooks=books
       }
-
 			showingBooks.sort(sortBy('authors'))
-
 		return (
 			<div className="search-books">
 					<div className="search-books-results">
@@ -45,8 +43,8 @@ class ListBooks extends Component{
                           </div>
                               <div className="book-shelf-changer">
 		                              <select onChange={(event) => {this.props.updateBook(book, event.target.value);}}>
-		                                <option value="nona" disabled>Move to...</option>
-                                    <option> </option>
+		                                <option value="none" disabled>Move to...</option>
+                                    <option value="none"> current shelf: {book.shelf} </option>
 																		<option value="currentlyReading">Currently Reading</option>
 		                                <option value="wantToRead">Want to Read</option>
 		                                <option value="read">Read</option>
